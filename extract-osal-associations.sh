131d01d4995487a86b7d010a7205d63d776ef36f
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-11-07 10:04:33 -0600

    allow extraction of multiple associations for a specific specimen

--- extract-osal-associations.sh
+++ extract-osal-associations.sh
@@ -9,20 +9,37 @@ extract_collecting_unit_id() {
 }
 
 extract_association_table() {
-  preston cat $1\
-  | xmllint --html --xmlout --xpath "//div[@id='unvouchered-association']/table" -\
-  | html2text\
-  | tail -n+2\
-  | sed "s+^+$1\t$2\t+g"\
-  | sed "s/|[ ]/\t/g"
+  local targetNames=$(preston cat $1\
+  | xmllint --html --xmlout --xpath '//div[@id="unvouchered-association"]//tbody/tr/td[position() mod 2 = 1]//text()' -\
+  | sed 's+\([^|]*\)[|].*+\1+g'\
+  | sed  '/^$/d')
+
+  local interactionTypesWithContext=$(preston cat $1\
+  | xmllint --html --xmlout --xpath '//div[@id="unvouchered-association"]//tbody/tr/td[position() mod 2 = 0]//text()' -\
+  | sed '/^$/d'\
+  | sed "s+^+https://linker.bio/$1\thttps://mbd-db.osu.edu/hol/collecting_units/$2\t+g")
+
+  paste <(echo "$interactionTypesWithContext") <(echo "$targetNames")\
+  | sed '/^$/d'
 }
 
+extract_host_vouchered_associations() {
+  preston cat $1\
+  | xmllint --html --xmlout --xpath "//h3[@aria-controls='#collecting_unit-association_parents']" -
+}
+
+extract_parasite_vouchered_associations() {
+  preston cat $1\
+  | xmllint --html --xmlout --xpath "//h3[@aria-controls='#collecting_unit-association_children']" -
+}
 
 extract_associations() {
   local contentId=$1
   unit_id=$(extract_collecting_unit_id $contentId)
   extract_association_table $contentId $unit_id
 }
+
+echo -e "contentId\tspecimenId\tinteractionTypeName\ttargetTaxonName\n"
 
 for contentId in $(preston ls\
  | grep "hol/collecting_units"\
